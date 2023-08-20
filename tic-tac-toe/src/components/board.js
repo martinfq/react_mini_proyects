3df5a7299a6d95b4422ef0e1b046c0f2d135a2a1
laptop_martin <martin.fq1908@gmail.com>
2023-08-19 23:37:55 -0500

    continuando

--- tic-tac-toe/src/components/board.js
+++ tic-tac-toe/src/components/board.js
@@ -1,3 +1,31 @@
-export default function Board(){
-    return <div>Board</div>
+import { useState } from "react";
+import Square from "./square";
+
+export default function Board() {
+
+    const [valor, setValor] = useState([]);
+  
+    function handleClick(){
+        const temp = [...valor];
+        asignarValor(temp);
+        console.log(valor);
+        setValor(temp);
+    }
+    function asignarValor(array){
+        if(array[array.length - 1] === 1){
+            array.push(0);
+        }else{
+            array.push(1);
+        }
+    }
+    
+    return (
+    <>
+      <div>
+        <Square valor={valor[0]} handleClick={handleClick}/>
+        <Square valor={valor[1]} handleClick={handleClick}/>
+        <Square valor={valor[2]} handleClick={handleClick}/>
+      </div>
+    </>
+  );
 }
---
+++ tic-tac-toe/src/components/square.js
@@ -0,0 +1,3 @@
+export default function Square({valor, handleClick}){
+    return  <button onClick={handleClick}>{valor}</button>
+}
